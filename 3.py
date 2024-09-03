e2c1fe7884f94be2a0604fd17d71f774edf7a369
Vitor Kenji <viitor.kenji@gmail.com>
2024-09-03 16:26:18 -0300

    finish challenge and upload json and xml files

--- 3.py
+++ 3.py
@@ -1,40 +1,46 @@
-# Nao encontrei o json ou xml que era para ser usado
+import json
+import xml.etree.ElementTree as ET
 
-import random
-
-def create_faturamentos():
-    out = []
-
-    for dia in range(1, 31):
-        if dia in (6, 7, 13, 14, 20, 21, 27, 28): # finais de semana
-            out.append(0)
-        else:
-            out.append(random.random() * random.randint(1e3, 1e6))
-
-    out[11] = 0 # simular feriado
-
-    return out
-
-def main():
-    faturamentos = create_faturamentos()
-    min, max = faturamentos[0], faturamentos[0]
+def info_faturamento(faturamentos):
+    min, max = float('inf'), float('-inf')
     dias_do_mes = 0
+    faturamento_total = 0
     for faturamento in faturamentos:
-        if (faturamento != 0):
+        if (faturamento['valor'] != 0):
             dias_do_mes += 1
-            if (faturamento < min):
-                min = faturamento
-            elif (faturamento > max):
-                max = faturamento
+            faturamento_total += faturamento['valor']
+            if (faturamento['valor'] < min):
+                min = faturamento['valor']
+            elif (faturamento['valor'] > max):
+                max = faturamento['valor']
 
-    faturamento_medio = sum(faturamentos) / dias_do_mes
+    faturamento_medio = faturamento_total / dias_do_mes
     dias_maior_faturamento_medio = 0
     for faturamento in faturamentos:
-        if faturamento != 0 and faturamento > faturamento_medio:
+        if faturamento['valor'] > faturamento_medio:
             dias_maior_faturamento_medio += 1
 
     print(f'Menor faturamento {min}')
     print(f'Maior faturamento {max}')
     print(f'Numero de dias com faturamento maior do que a media mensal {dias_maior_faturamento_medio}')
+
+def main():
+    with open('dados.json') as data:
+        dados_json = json.load(data)
+
+    tree = ET.parse('dados.xml')
+    root = tree.getroot()
+    dados_xml = []
+    for row in root:
+        full_data = {}
+        for data in row:
+            full_data[data.tag] = float(data.text)
+        dados_xml.append(full_data)
+
+    print('Para o arquivo dados.json')
+    info_faturamento(dados_json)
+
+    print('Para o arquivo dados.xml')
+    info_faturamento(dados_xml)
 
 main()
